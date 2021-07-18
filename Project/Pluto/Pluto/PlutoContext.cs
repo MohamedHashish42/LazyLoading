e3803db58885929314bcddb0375a5954e4fb2657
MohamedHashish42 <mohamed.hashish42@gmail.com>
2021-07-18 23:19:33 +0200

    Add project files.

---
+++ Project/Pluto/Pluto/PlutoContext.cs
@@ -0,0 +1,20 @@
+﻿
+using Pluto.Entities;
+using System.Data.Entity;
+
+namespace Pluto
+{
+
+    public class PlutoContext : DbContext
+    {
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+        public PlutoContext() : base("name=DefaultConnection")
+        {
+
+        }
+    }
+}
