386679690bf2123481aece978b955a5b8cc9b81e
mohamedhashish123 <m.hashish@excprotection.com>
2021-07-18 23:19:33 +0200

    Add project files.

---
+++ Project/Pluto/Pluto/Entities/Tag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pluto.Entities
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IList<Course> Courses { get; set; }
+    }
+}
---
+++ Project/Pluto/Pluto/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pluto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PlutoContext context = new PlutoContext();
+
+            // Loading Author Data Only Wihtout His Courses 
+
+            var Author = context.Authors.Find(1);
+
+            // Loads The Courses For Particular Author only (in a separate SQL query)
+
+            var Courses = Author.Courses;
+
+
+        }
+    }
+}
